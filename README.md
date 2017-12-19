8f584864cf471945db3532db041f0ab1eae61174
Parik <parikshitd92@gmail.com>
2017-12-19 09:26:18 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# todo-list-js
+Blog post tutorial writeup: http://seanoneill.me/yet-another-javascript-todo-list-tutorial/
+
+Demo: http://seanoneill.me/demos/todo-list-js/index.html
---
+++ js/main.js
@@ -0,0 +1,52 @@
+document.getElementById('todolist').addEventListener('submit', saveToDo);
+
+function saveToDo(event){
+	var d = new Date()
+	  event.preventDefault();
+	var getId =  d.getTime();
+	var getInput = document.getElementById("todobox").value;
+	var todo = {id: getId, todo: getInput}
+  
+   if(localStorage.getItem('toDoList') === null){
+	     var toDoList = [];
+		 toDoList.push(todo);
+		 localStorage.setItem('toDoList', JSON.stringify(toDoList));
+   }else{
+	   var toDoList = JSON.parse(localStorage.getItem('toDoList'));
+	    toDoList.push(todo);
+		 localStorage.setItem('toDoList', JSON.stringify(toDoList));
+   }
+      
+	    document.getElementById('todolist').reset();
+    	getToDo();
+
+}
+
+function getToDo(){
+	var toDoList = JSON.parse(localStorage.getItem('toDoList'));
+	var ShowToDo = document.getElementById('ShowToDoList');
+	ShowToDoList.innerHTML = ''; 
+	for(var i = 0; i < toDoList.length; i++){
+		var id = toDoList[i].id;
+		var toDoText = toDoList[i].todo;
+		
+		ShowToDoList.innerHTML +=  
+		'<hr>'+     
+		'<p>' +toDoText+'</p>'+
+		'<ul> <a href="#" onclick="deleteToDo(\''+id+'\')">Done!</a> </ul>';
+	}
+}
+
+	function deleteToDo(id){
+		var toDoList = JSON.parse(localStorage.getItem('toDoList'));
+		for(var i=0; i < toDoList.length; i++){
+			if(toDoList[i].id == id){
+				toDoList.splice(i,1);
+			}
+		}
+		localStorage.setItem('toDoList', JSON.stringify(toDoList));
+		getToDo();
+	}
+
+
+	
